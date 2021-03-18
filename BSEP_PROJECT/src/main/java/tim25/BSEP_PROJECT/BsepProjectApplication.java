feef135c547eba24df42ca46e1010cb99b955c55
Jovana Jakovljevic <jovanajako@hotmail.com>
2021-03-18 19:51:34 +0100

    added html page for creating certificate

---
+++ BSEP_PROJECT/src/main/java/tim25/BSEP_PROJECT/BsepProjectApplication.java
@@ -0,0 +1,13 @@
+package tim25.BSEP_PROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsepProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsepProjectApplication.class, args);
+	}
+
+}
---
+++ BSEP_PROJECT/src/test/java/tim25/BSEP_PROJECT/BsepProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tim25.BSEP_PROJECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsepProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
