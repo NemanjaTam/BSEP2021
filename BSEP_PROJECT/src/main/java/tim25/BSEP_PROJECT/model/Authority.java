252345af7f7bd8c75360c7bd7c0f1fa04a66a205
NemanjaTam <57495945+NemanjaTam@users.noreply.github.com>
2021-04-25 13:51:12 +0200

    model

--- BSEP_PROJECT/src/main/java/tim25/BSEP_PROJECT/model/Authority.java
+++ BSEP_PROJECT/src/main/java/tim25/BSEP_PROJECT/model/Authority.java
@@ -1,15 +1,12 @@
 package tim25.BSEP_PROJECT.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import org.springframework.security.core.GrantedAuthority;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "authority")
@@ -23,7 +20,15 @@ public class Authority implements GrantedAuthority {
     Long id;
 
     @Column(name = "name")
-    String name;
+    String name; //rola
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "authority_privileges", joinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "privilege_id", referencedColumnName = "id"))
+    private Set<Privilege> privileges;
+
+    @ManyToMany(mappedBy = "authorities" , fetch = FetchType.LAZY)
+    private Set<User> users;
 
     @Override
     public String getAuthority() {
@@ -46,6 +51,14 @@ public class Authority implements GrantedAuthority {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Set<Privilege> getPrivileges() {
+        return this.privileges;
+    }
+
+    public void setPrivileges(Set<Privilege> privileges) {
+        this.privileges = privileges;
     }
 
 }
