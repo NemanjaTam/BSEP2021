c665aa314932e067cf1bd2cb551c046da480414a
Nemanja Tamindzija <nemanja.tam69@gmail.com>
2021-06-11 16:45:21 +0200

    config guards added

---
+++ security-backend/src/main/java/com/project/securitybackend/config/SimpleCorsFilter.java
@@ -0,0 +1,45 @@
+package com.project.securitybackend.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class SimpleCorsFilter implements Filter {
+
+    public SimpleCorsFilter() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("X-XSS-Protection", "1;mode=block");
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        // Auto-generated method stub
+
+    }
+
+    @Override
+    public void init(FilterConfig arg0) {
+        // Auto-generated method stub
+
+    }
+}
